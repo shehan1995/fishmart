a10a9c7af2945874619c1d32011a1e2b8723ac52
neokoder <neokoder@hotmail.com>
2020-04-24 22:37:38 +0530

    Create 2019_04_19_171251_create_fish_table.php

---
+++ database/migrations/2019_04_19_171251_create_fish_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFishTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fish', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('name');
+            $table->float('avg_price');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fish');
+    }
+}
