80c80d8c9a5cb8ad634a3782b9bf56239bd23dc4
neokoder <neokoder@hotmail.com>
2020-04-24 22:38:31 +0530

    Update 2020_04_07_120630_create_orders_table.php

--- database/migrations/2020_04_07_120630_create_orders_table.php
+++ database/migrations/2020_04_07_120630_create_orders_table.php
@@ -17,8 +17,8 @@ class CreateOrdersTable extends Migration
             $table->bigIncrements('id')->unique();
             $table->unsignedBigInteger('selling_id');
             $table->foreign('selling_id')->references('id')->on('selling_a_d_s');
-            $table->unsignedBigInteger('buying_id');
-            $table->foreign('buying_id')->references('id')->on('buying_a_d_s');
+            $table->unsignedBigInteger('buyer_id');
+            $table->foreign('buyer_id')->references('id')->on('users');
             $table->float('amount');
             $table->string('status');
             $table->timestamps();
