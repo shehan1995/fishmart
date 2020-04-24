0bc3c7072318c3363d474122392e97918adca065
neokoder <neokoder@hotmail.com>
2020-04-24 22:37:35 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('categary');
-            $table->mediumText('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
