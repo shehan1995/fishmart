a62099d85f3505056738ae844b76ca9048554fc7
Shehan Rajakaruna <shehanr@pickme.lk>
2020-10-21 13:07:05 +0530

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('categary');
+            $table->string('image');
             $table->rememberToken();
             $table->timestamps();
         });
